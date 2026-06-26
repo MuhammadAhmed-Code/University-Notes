b9f3f9ca264cd04ea9211e611e00312a83d5de82
Muhammad Ahmed <bscs2412115@szabist.pk>
2026-06-26 18:56:34 +0500

    Update Main.java

--- Design&AlgorithmAnalysis/Assignment02/DesignAndAlgorithmAnalysis/src/Main.java
+++ Design&AlgorithmAnalysis/Assignment02/DesignAndAlgorithmAnalysis/src/Main.java
@@ -1,5 +1,16 @@
-class Main{
-    public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
 
+        Thread t1 = new Thread(() -> QuestionONE.main(null), "EdmondsKarp-Thread");
+        Thread t2 = new Thread(() -> QuestionTWO.main(null),  "BoyerMoore-Thread");
+        Thread t3 = new Thread(() -> QuestionTHREE.main(null),    "Dijkstra-Thread");
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
     }
 }
